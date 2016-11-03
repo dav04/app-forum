03a45408466d5d2a225d004f2010a27ee1a94ac8
Dav04 <davidemedina7@gmail.com>
2016-11-03 17:36:40 +0100

    Aggiunta file index e components

---
+++ index.php
@@ -0,0 +1,14 @@
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, minimum-scale=1.0, initial-scale=1, user-scalable=yes">
+		
+		<title>Forum</title>
+		
+		<script src="components/bower_components/webcomponentsjs/webcomponents-lite.js"></script>
+		<link rel="import" href="components/components.html">
+	</head>
+	<body>
+
+	</body>
+</html>
